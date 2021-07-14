edd4043e8d71ff59657f71cea3065f8e0b4d5389
nrzhnvdn <nurzhanovadana9779@gmail.com>
2021-07-15 00:39:16 +0600

    first hw

---
+++ main.py
@@ -0,0 +1,20 @@
+# 0 урок
+print("Hello world")
+# end 0 урок
+# homework 1 week
+# 2 пункт
+print("Hello I am programmer")
+# 3 пункт
+Name = "Dana"
+print(Name)
+print("My name is "+Name)
+# 4 пункт
+a="103"
+b=301
+a=int(a)
+Not_found=a+b
+print(Not_found)
+print(b+int(a))
+# 5 пункт
+
+# end homework 1 week
