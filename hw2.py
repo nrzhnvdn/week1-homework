6d1b7c1263aaee9a287a1df6f4cc3cba226bf305
nrzhnvdn <87442929+nrzhnvdn@users.noreply.github.com>
2021-07-16 19:44:23 +0600

    Add files via upload
    homework 2

---
+++ hw2.py
@@ -0,0 +1,31 @@
+#1
+a="2.1"
+a=float(a)
+b="1.04"
+b=float(b)
+c=a+b
+print (c)
+#2
+a = bool("Hello")
+print(a)
+b = bool(123)
+print(b)
+c = bool(0)
+print(c)
+d = bool(-1)
+print(d)
+f = bool("-1")
+print(f)
+#3
+print('I','like','Python', sep='***',end="-")
+#4
+#sep между строк, end в конце добавляет символы
+#5
+#-50
+#6
+a=int(input(''))
+b=int(input(''))
+c=int(input(''))
+d=int(input(''))
+cost=(a+b+c+d)*3
+print (cost)
